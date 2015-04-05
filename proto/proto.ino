89a23ecbf0f91d71a812e59d5df7f36ff0b26b4f
lucasdenefle <denefle@ece.fr>
2015-04-05 13:55:35 +0200

    Ajout des fonctions de test

--- proto/proto.ino
+++ proto/proto.ino
@@ -1,7 +1,7 @@
 #include <QueueArray.h>
 #include <CapacitiveSensor.h>
 
-#define SIZE 8 
+#define SIZE 8
 //#define SIZE 2
 #define SENSITIVITY 3
 #define PRECISION 30
@@ -18,41 +18,43 @@ struct deplacement
 };
 
 
-enum state_enum{
-   LISTEN,
-   SLEEP,
-   RESET,
-   WAITING,
-   VALID,
-   INVALID,
-   UPDATE,
-   TEST,
-   INIT
+enum state_enum {
+  LISTEN,
+  SLEEP,
+  RESET,
+  WAITING,
+  VALID,
+  INVALID,
+  UPDATE,
+  TEST,
+  INIT,
+  TEST_LED,
+  TEST_CAPT
 };
 
 
 long moyenne_array[PRECISION];
-long releve=0;
-int index=0;
+long releve = 0;
+int index = 0;
 char inChar;
 char inData[SERIAL_SIZE] = "";
-int player=1;
+int player = 1;
 
 state_enum state = INIT;
- 
- 
-CapacitiveSensor   cs_4_2 = CapacitiveSensor(5,7);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
 
-int inp[SIZE] = { 41,45,49,53,51,47,43,39 };
-int LED[2]={15,17};
+
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(5, 7);       // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+
+int inp[SIZE] = { 41, 45, 49, 53, 51, 47, 43, 39 };
+int LED[2] = {15, 17};
 //int inp[SIZE] = { 46, 42};
-int outp[SIZE] = { 35,31,27,23,25,29,33,37};
+int outp[SIZE] = { 35, 31, 27, 23, 25, 29, 33, 37};
 //int outp[SIZE] = { 38,34};
-int tab[SIZE][SIZE] = {{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1}};
+int tab[SIZE][SIZE] = {{1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}};
 //int tab[SIZE][SIZE] = {{1,1},{1,1}};
 //int oldtab[SIZE][SIZE] = {{1,1},{1,1}};
 int goodtab[SIZE][SIZE];
-int oldtab[SIZE][SIZE]  = {{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1}};
+int oldtab[SIZE][SIZE]  = {{1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}};
 QueueArray<deplacement> array;
 
 
@@ -63,173 +65,187 @@ boolean stringComplete = false;  // whether the string is complete
 
 void setup()
 {
-       
+
   Serial.begin(9600);
-   
-   //Declaration OUTPUT et INPUT de la matrice
-   for (int i = 0; i<SIZE; i++)
+
+  //Declaration OUTPUT et INPUT de la matrice
+  for (int i = 0; i < SIZE; i++)
   {
     pinMode(inp[i], INPUT_PULLUP);
     pinMode(outp[i], OUTPUT);
     digitalWrite(outp[i], HIGH);
   }
-  
-  //Declaration PIN des LEDs
-  for(int i=0;i<2;i++)
-  {
-    pinMode(LED[i],OUTPUT);
-    digitalWrite(LED[i],LOW);
-  }
-  
 
-      cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);  //Calibrate the sensor... 
-      // reserve 200 bytes for the inputString:
-      inputString.reserve(200);
-        
-     if(state!=TEST){
-      init_array();
-     }
+  //Declaration PIN des LEDs
+  for (int i = 0; i < 2; i++)
+  {
+    pinMode(LED[i], OUTPUT);
+    digitalWrite(LED[i], LOW);
+  }
+
+
+  cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);  //Calibrate the sensor...
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+
+  if (state != TEST) {
+    init_array();
+  }
 }
 
 
 void loop()
 {
-    if (stringComplete) {
-   // Serial.println(inputString);
+  if (stringComplete) {
+    // Serial.println(inputString);
     // clear the string:
-   
-         if(inputString=="sleep\n")
-        {state=SLEEP;
-        }
-        if(inputString=="listen 1\n")
-        {
-          player=0;
-          turn_off_all_leds();
-          turn_on_led();
-          state=LISTEN;
-        }
-        if(inputString=="listen 2\n")
-        {
-          player=1;
-          turn_off_all_leds();
-          turn_on_led();
-          state=LISTEN;
-        }
-        if(inputString=="valid\n")
-        {state=VALID;
-        }
-        if(inputString=="invalid\n")
-        {state=INVALID;
-        }
-        if(inputString=="update\n")
-        {state=UPDATE;
-        }
-        if(inputString=="waiting\n")
-        {state=WAITING;
-        } 
-        if(inputString=="test\n")
-        {state=TEST;
-        }
-        if(inputString=="init\n")
-        {state=INIT;
-        }
-        
-        
-     inputString = "";
-     stringComplete = false;
+
+    if (inputString == "sleep\n")
+    { state = SLEEP;
+    }
+    if (inputString == "listen 1\n")
+    {
+      player = 0;
+      turn_off_all_leds();
+      turn_on_led();
+      state = LISTEN;
+    }
+    if (inputString == "listen 2\n")
+    {
+      player = 1;
+      turn_off_all_leds();
+      turn_on_led();
+      state = LISTEN;
+    }
+    if (inputString == "valid\n")
+    { state = VALID;
+    }
+    if (inputString == "invalid\n")
+    { state = INVALID;
+    }
+    if (inputString == "update\n")
+    { state = UPDATE;
+    }
+    if (inputString == "waiting\n")
+    { state = WAITING;
+    }
+    if (inputString == "test\n")
+    { state = TEST;
+    }
+    if (inputString == "init\n")
+    { state = INIT;
+    }
+    if (inputString == "test_led\n")
+    { state = TEST_LED;
+    }
+    if (inputString == "test_capt\n")
+    { state = TEST_CAPT;
+    }
+
+
+    inputString = "";
+    stringComplete = false;
   }
 
-  switch(state){
-       case UPDATE:
-         //Scan la matrice et remplit goodtab
-         //Serial.println("update");
-         scantab(goodtab);
-         break;
-         
-       case INIT:
-         scantab(goodtab);
-         scantab(oldtab);
-         scantab(tab);
-         //printtab(goodtab);
-         state=SLEEP;
-         break;
-       case LISTEN: 
-         //Serial.println("listen");
-         listen();
-         //CAPTEUR TEST     
-         if(getreleve() > 2* getmoyenne() && !array.isEmpty()){
-         //Serial.println("TOUCHE");
-         pop_array();}
-         break;
-       case SLEEP:
-        //Serial.println("sleep");
-         sleepfunction();
-         break;
-       case VALID:
-         //valide le move 
-         //Serial.println("valid");
-         scantab(goodtab);
-         turn_off_all_leds();
-         state=SLEEP;
-         break;
-       case INVALID:
-       //  Serial.println("invalid");
-         //attend que le tableau revienne à son dernier état correct
-         
-         
-         if(!isValidTab()){
-       //    Serial.println("TAB VALIDE");
-           delay(1000);
-           state=LISTEN;
-           scantab(oldtab);
-           scantab(tab);
-         }
-         else{//blink
-           blink_led();
-         }
-         break;
-       case WAITING:
-         // Serial.println("update");
-         //attend que le rasp communique "valid" ou "invalid"
-         break;
-       case TEST:
-       //  listen_test();
-         //delay(500);
-         blink_led();
-         turn_off_all_leds();
-         delay(1000);
-         switch_player();
-       break;
-       
+  switch (state) {
+  case UPDATE:
+    //Scan la matrice et remplit goodtab
+    //Serial.println("update");
+    scantab(goodtab);
+    break;
+
+  case INIT:
+    scantab(goodtab);
+    scantab(oldtab);
+    scantab(tab);
+    //printtab(goodtab);
+    state = SLEEP;
+    break;
+  case LISTEN:
+    //Serial.println("listen");
+    listen();
+    //CAPTEUR TEST
+    if (getreleve() > 2 * getmoyenne() && !array.isEmpty()) {
+      //Serial.println("TOUCHE");
+      pop_array();
+    }
+    break;
+  case SLEEP:
+    //Serial.println("sleep");
+
+    break;
+  case VALID:
+    //valide le move
+    //Serial.println("valid");
+    scantab(goodtab);
+    turn_off_all_leds();
+    state = SLEEP;
+    break;
+  case INVALID:
+    //  Serial.println("invalid");
+    //attend que le tableau revienne à son dernier état correct
+
+
+    if (!isValidTab()) {
+      //    Serial.println("TAB VALIDE");
+      delay(1000);
+      state = LISTEN;
+      scantab(oldtab);
+      scantab(tab);
+    }
+    else { //blink
+      blink_led();
+    }
+    break;
+  case WAITING:
+    // Serial.println("update");
+    //attend que le rasp communique "valid" ou "invalid"
+    break;
+  case TEST:
+    listen_test();
+    //delay(500);
+
+    break;
+
+  case TEST_LED:
+    blink_led();
+    turn_off_all_leds();
+    delay(1000);
+    switch_player();
+    break;
+  case TEST_CAPT:
+    if (getreleve() > 2 * getmoyenne()) {
+      Serial.println("TOUCHE");
+    }
+    break;
   }
-  
+
   //capteur calibration
-     shift_array();
-     moyenne_array[PRECISION-1]=cs_4_2.capacitiveSensor(30);    
-   
+  shift_array();
+  moyenne_array[PRECISION - 1] = cs_4_2.capacitiveSensor(30);
 
 
-  
+
+
 }
-void sleepfunction(){
-}
+
 
 void listen()
 {
-    scantab(tab); 
-     if(comptab(oldtab, tab))
+  scantab(tab);
+  if (comptab(oldtab, tab))
   { printtypecoup(oldtab, tab);
     copytab(oldtab, tab);
     delay(500);
-  }        
+  }
 }
 
 
 void listen_test()
 {
-    
-  scantab(tab);   
-   if(comptab(oldtab, tab))
+
+  scantab(tab);
+  if (comptab(oldtab, tab))
   {
     printtab(tab);
     printtypecoup(oldtab, tab);
@@ -239,20 +255,20 @@ void listen_test()
 
 void scantab(int tab[SIZE][SIZE])
 {
-  
-      for (int j = 0; j<SIZE; j++)
-    {   for (int h = 0; h<SIZE; h++)
-      { 
-        digitalWrite(outp[j], LOW);                        
-        if (digitalRead(inp[h]) == LOW)
-        { tab[h][j] = 0;
-        }
-        else
-        { tab[h][j] = 1;
-        }
-        digitalWrite(outp[j], HIGH);
-       }
-     }
+
+  for (int j = 0; j < SIZE; j++)
+  { for (int h = 0; h < SIZE; h++)
+    {
+      digitalWrite(outp[j], LOW);
+      if (digitalRead(inp[h]) == LOW)
+      { tab[h][j] = 0;
+      }
+      else
+      { tab[h][j] = 1;
+      }
+      digitalWrite(outp[j], HIGH);
+    }
+  }
 
 }
 
@@ -263,9 +279,9 @@ void printtab(int tab[SIZE][SIZE])
 //Affiche un tableau en argument
 {
   Serial.println("printtab");
-  for (int j = 0; j<SIZE; j++)
+  for (int j = 0; j < SIZE; j++)
   {
-    for (int h = 0; h<SIZE; h++)
+    for (int h = 0; h < SIZE; h++)
     {
       Serial.print(tab[h][j]);
     }
@@ -280,35 +296,35 @@ void printtab(int tab[SIZE][SIZE])
 void printtypecoup(int tab1[SIZE][SIZE], int tab2[SIZE][SIZE])
 //Ajoute un coup à la file
 {
-        deplacement temp;                 
-       
-  for (int j = 0; j<SIZE; j++)
+  deplacement temp;
+
+  for (int j = 0; j < SIZE; j++)
   {
-    for (int h = 0; h<SIZE; h++)
+    for (int h = 0; h < SIZE; h++)
     {
       if (tab1[h][j] != tab2[h][j])
       {
-        
+
         if (tab1[h][j])
         {
-           //Serial.println("AJOUT Piece posee en ");
-          temp.type='d';                                 
+          //Serial.println("AJOUT Piece posee en ");
+          temp.type = 'd';
         }
         else
-        {       
-           //Serial.println("AJOUT Piece levee en:");
-           temp.type='u';
+        {
+          //Serial.println("AJOUT Piece levee en:");
+          temp.type = 'u';
 
         }
-                                temp.x=h;
-                                temp.y=j;
-                                array.push(temp);
-/*
-        Serial.print("x = ");
-        Serial.println(h);
-        Serial.print("y = ");
-        Serial.println(j);
-        Serial.println();*/
+        temp.x = h;
+        temp.y = j;
+        array.push(temp);
+        /*
+                Serial.print("x = ");
+                Serial.println(h);
+                Serial.print("y = ");
+                Serial.println(j);
+                Serial.println();*/
       }
 
     }
@@ -321,9 +337,9 @@ void printtypecoup(int tab1[SIZE][SIZE], int tab2[SIZE][SIZE])
 void copytab(int tab1[SIZE][SIZE], int tab2[SIZE][SIZE])
 // Copie tab1 et tab2
 {
-  for (int j = 0; j<SIZE; j++)
+  for (int j = 0; j < SIZE; j++)
   {
-    for (int h = 0; h<SIZE; h++)
+    for (int h = 0; h < SIZE; h++)
     {
       tab1[h][j] = tab2[h][j];
 
@@ -338,9 +354,9 @@ boolean comptab(int tab1[SIZE][SIZE], int tab2[SIZE][SIZE])
 //retourne 1 en cas de non correspondance des tableaux
 //0 en cas de correspondance
 {
-  for (int j = 0; j<SIZE; j++)
+  for (int j = 0; j < SIZE; j++)
   {
-    for (int h = 0; h<SIZE; h++)
+    for (int h = 0; h < SIZE; h++)
     {
       if (tab1[h][j] != tab2[h][j])
       {
@@ -359,23 +375,23 @@ boolean isValidTab()
 {
   int temptab[SIZE][SIZE];
   scantab(temptab);
- // printtab(temptab);
-  if(comptab(temptab,goodtab)){
-       //Tableaux différents
-       return true;
-   }
-  else{ //Tableaux identiques
-      return false;
-    }
-    
-    delay(500);
-  
+// printtab(temptab);
+  if (comptab(temptab, goodtab)) {
+    //Tableaux différents
+    return true;
+  }
+  else { //Tableaux identiques
+    return false;
+  }
+
+  delay(500);
+
 }
 
 
 //SERIAL
 /****************************************************/
-//S'execute entre chaque tour de loop 
+//S'execute entre chaque tour de loop
 void serialEvent() {
   //Serial.println("serialEvent");
   while (Serial.available()) {
@@ -387,7 +403,7 @@ void serialEvent() {
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
-        stringComplete = true;
+      stringComplete = true;
     }
   }
 }
@@ -395,27 +411,27 @@ void serialEvent() {
 //Depile la liste de coups dans serial
 void pop_array()
 {
-  
-  deplacement a={'l',1,1};
+
+  deplacement a = {'l', 1, 1};
   char x;
   while (!array.isEmpty ())
   {
-    a=array.pop();   
-Serial.print (a.type);
-x=numbertoletter(a.x);
-Serial.print (x);
-Serial.print( 8-(a.y));
-Serial.print(" ");
-}
-   state=WAITING;
-    Serial.println();   
+    a = array.pop();
+    Serial.print (a.type);
+    x = numbertoletter(a.x);
+    Serial.print (x);
+    Serial.print( 8 - (a.y));
+    Serial.print(" ");
+  }
+  state = WAITING;
+  Serial.println();
 
-delay(100);    
-                               
+  delay(100);
+
 }
 
-char numbertoletter(int a){
-    return 'a'+(7-a);
+char numbertoletter(int a) {
+  return 'a' + (7 - a);
 }
 
 /****************************************************/
@@ -428,14 +444,14 @@ char numbertoletter(int a){
 //Fait la moyenne sur toutes l'array de relevés
 long getmoyenne()
 {
-  int i=0;
-  long moyenne=0;
-  for(i=0;i<PRECISION-PRECISION_MOYENNE;i++)
+  int i = 0;
+  long moyenne = 0;
+  for (i = 0; i < PRECISION - PRECISION_MOYENNE; i++)
   {
-    moyenne+=moyenne_array[i];
-    
+    moyenne += moyenne_array[i];
+
   }
-  moyenne=moyenne/(PRECISION-PRECISION_MOYENNE);
+  moyenne = moyenne / (PRECISION - PRECISION_MOYENNE);
   //Serial.print("MOYENNE : ");
   //Serial.println(moyenne);
   return moyenne;
@@ -445,13 +461,13 @@ long getmoyenne()
 //Fait une moyenne des 5 derniers relevés
 long getreleve()
 {
-  int i=0;
-  long releve=0;
-  for(i=PRECISION-PRECISION_MOYENNE;i<PRECISION;i++)
+  int i = 0;
+  long releve = 0;
+  for (i = PRECISION - PRECISION_MOYENNE; i < PRECISION; i++)
   {
-    releve=moyenne_array[i]+releve;
+    releve = moyenne_array[i] + releve;
   }
-  releve=releve/5;
+  releve = releve / 5;
   //Serial.print("RELEVE : ");
   //Serial.println(releve);
   return releve;
@@ -459,23 +475,23 @@ long getreleve()
 
 
 
-//Décale l'array de moyennes 
+//Décale l'array de moyennes
 void shift_array()
 {
-  int i=0;
-  for(i=0;i<PRECISION-1;i++)
+  int i = 0;
+  for (i = 0; i < PRECISION - 1; i++)
   {
-    moyenne_array[i]=moyenne_array[i+1];
+    moyenne_array[i] = moyenne_array[i + 1];
   }
 }
 
 //Nettoie l'array
 void init_array()
 {
-   for (int i=0;i<PRECISION;i++)
-  { 
-  moyenne_array[i]=cs_4_2.capacitiveSensor(30);
-  delay(5);
+  for (int i = 0; i < PRECISION; i++)
+  {
+    moyenne_array[i] = cs_4_2.capacitiveSensor(30);
+    delay(5);
   }
 }
 
@@ -489,39 +505,39 @@ void init_array()
 
 /****************************************************/
 void turn_off_all_leds()
-{  
-  for(int i=0;i<2; i++)
+{
+  for (int i = 0; i < 2; i++)
   {
-    digitalWrite(LED[i],LOW);
+    digitalWrite(LED[i], LOW);
   }
 }
 
 void turn_on_led()
 {
-  digitalWrite(LED[player],HIGH);
+  digitalWrite(LED[player], HIGH);
 }
 
 void turn_off_led()
 {
-  digitalWrite(LED[player],LOW);
+  digitalWrite(LED[player], LOW);
 }
 
 void blink_led()
 {
-  
-    digitalWrite(LED[player],LOW);
-    delay(500);
-    digitalWrite(LED[player],HIGH);
-    delay(500);
-  
+
+  digitalWrite(LED[player], LOW);
+  delay(500);
+  digitalWrite(LED[player], HIGH);
+  delay(500);
+
 }
 
 void switch_player()
 {
-  if(player==1)
-    {
-       player=0;
-    }
-  else{player=1;}
+  if (player == 1)
+  {
+    player = 0;
+  }
+  else {player = 1;}
 }
 /****************************************************/
